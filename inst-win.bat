d04d0b1135c247fe8116ed5f6d7aaa72091fb18a
Yangsy56302 <yangsy56302@163.com>
2025-02-03 18:22:34 +0800

    4.104 update

--- inst-win.bat
+++ inst-win.bat
@@ -1,5 +1,5 @@
 pip install -r requirements.txt
 set PYINST=TRUE
-pyinstaller --specpath . --distpath . --workpath ./temp/make --onefile --console --icon logo/a2icon.ico --name bmp bmp.py
-pyinstaller --specpath . --distpath . --workpath ./temp/make --onefile --windowed --icon logo/a2icon.ico --name submp submp.py
+pyinstaller --specpath . --distpath . --workpath ./temp/make --onefile --console --icon logo/a8icon.ico --name bmp bmp.py
+pyinstaller --specpath . --distpath . --workpath ./temp/make --onefile --windowed --icon logo/c2icon.ico --name submp submp.py
 pause
